2b0cdcfbd4946401fb99ec5e4aa9300a3a38b859
HP Victus <minhynguyen97@gmail.com>
2024-08-01 23:37:52 +0700

    Part 3 progress

---
+++ Part3/csharp-evaluate-boolean-expressions/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+    Random coin = new Random();
+    Console.WriteLine((coin.Next(0, 2) == 0) ? "heads" : "tails");
+*/
+
+string permission = "Admin|Manager";
+int level = 60;
+
+if (permission.Contains("Admin"))
+{
+    if (level <= 55)
+    {
+        System.Console.WriteLine("Welcome, Admin user.");
+    }
+    System.Console.WriteLine("Welcome, Super Admin user.");
+}
+else
+{
+    if (level >= 20)
+    {
+        System.Console.WriteLine("Contact an Admin for access.");
+    }
+    System.Console.WriteLine("You do not have sufficient privileges.");
+}
